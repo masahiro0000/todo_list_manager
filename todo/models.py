3f56d4015a99a2a6dcaa64cb662aa786b8054165
masahiro0000 <aya0724masa@gmail.com>
2024-03-06 07:12:24 +0900

    todoモデル作成

---
+++ todo/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class todo:
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todo")
+    description = models.TextField(blank=True, null=True)
+    due_date = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
